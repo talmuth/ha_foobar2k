e88bc72b87dff58a00755d81e7258ac77fb11e59
Denys Lozko <talmuth@outlook.com>
2025-02-11 17:19:24 -0800

    Make it compatible with 2025.2

--- custom_components/foobar/media_player.py
+++ custom_components/foobar/media_player.py
@@ -1,4 +1,4 @@
-""" 
+"""
 Support for foobar2000 Music Player as media player
 via pyfoobar2k https://gitlab.com/ed0zer-projects/pyfoobar2k
 And foobar2000 component foo_httpcontrol by oblikoamorale https://bitbucket.org/oblikoamorale/foo_httpcontrol
@@ -14,52 +14,77 @@ import homeassistant.util.dt as dt_util
 try:
     from homeassistant.components.media_player import PLATFORM_SCHEMA, MediaPlayerEntity
 except ImportError:
-    from homeassistant.components.media_player import PLATFORM_SCHEMA, MediaPlayerDevice as MediaPlayerEntity
+    from homeassistant.components.media_player import (
+        PLATFORM_SCHEMA,
+        MediaPlayerDevice as MediaPlayerEntity,
+    )
 
 from homeassistant.components.media_player.const import (
-    MEDIA_TYPE_MUSIC, SUPPORT_VOLUME_STEP, SUPPORT_TURN_ON, SUPPORT_TURN_OFF,
-    SUPPORT_STOP, SUPPORT_SELECT_SOURCE, SUPPORT_SEEK, SUPPORT_VOLUME_SET, SUPPORT_VOLUME_MUTE,
-    SUPPORT_PLAY, SUPPORT_PREVIOUS_TRACK, SUPPORT_NEXT_TRACK, SUPPORT_PAUSE, SUPPORT_SHUFFLE_SET)
+    MediaType,
+    MediaPlayerEntityFeature,
+)
+
 
 from homeassistant.const import (
-    CONF_NAME, CONF_HOST, CONF_PORT, CONF_USERNAME,
-    CONF_PASSWORD, STATE_OFF, STATE_PAUSED, STATE_PLAYING,
-    CONF_TIMEOUT, STATE_UNKNOWN, STATE_IDLE)
+    CONF_NAME,
+    CONF_HOST,
+    CONF_PORT,
+    CONF_USERNAME,
+    CONF_PASSWORD,
+    STATE_OFF,
+    STATE_PAUSED,
+    STATE_PLAYING,
+    CONF_TIMEOUT,
+    STATE_UNKNOWN,
+    STATE_IDLE,
+)
 
-REQUIREMENTS = ['pyfoobar2k==0.2.8']
+REQUIREMENTS = ["pyfoobar2k==0.2.8"]
 
 SCAN_INTERVAL = timedelta(seconds=5)
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = 'Foobar2000'
-DEFAULT_PORT = '8888'
+DEFAULT_NAME = "Foobar2000"
+DEFAULT_PORT = "8888"
 DEFAULT_TIMEOUT = 3
 DEFAULT_VOLUME_STEP = 5
-CONF_VOLUME_STEP = 'volume_step'
-CONF_TURN_ON_ACTION = 'turn_on_action'
-CONF_TURN_OFF_ACTION = 'turn_off_action'
+CONF_VOLUME_STEP = "volume_step"
+CONF_TURN_ON_ACTION = "turn_on_action"
+CONF_TURN_OFF_ACTION = "turn_off_action"
 
-SUPPORT_FOOBAR_PLAYER = \
-    SUPPORT_PAUSE | SUPPORT_VOLUME_SET | SUPPORT_VOLUME_MUTE | \
-    SUPPORT_PLAY | SUPPORT_NEXT_TRACK | SUPPORT_PREVIOUS_TRACK | \
-    SUPPORT_STOP | SUPPORT_SEEK | SUPPORT_SHUFFLE_SET | \
-    SUPPORT_VOLUME_STEP | SUPPORT_SELECT_SOURCE
+SUPPORT_FOOBAR_PLAYER = (
+    MediaPlayerEntityFeature.PAUSE
+    | MediaPlayerEntityFeature.VOLUME_SET
+    | MediaPlayerEntityFeature.VOLUME_MUTE
+    | MediaPlayerEntityFeature.PLAY
+    | MediaPlayerEntityFeature.NEXT_TRACK
+    | MediaPlayerEntityFeature.PREVIOUS_TRACK
+    | MediaPlayerEntityFeature.STOP
+    | MediaPlayerEntityFeature.SEEK
+    | MediaPlayerEntityFeature.SHUFFLE_SET
+    | MediaPlayerEntityFeature.VOLUME_STEP
+    | MediaPlayerEntityFeature.SELECT_SOURCE
+)
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_HOST): cv.string,
-    vol.Optional(CONF_USERNAME): cv.string,
-    vol.Optional(CONF_PASSWORD): cv.string,
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
-    vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
-    vol.Optional(CONF_VOLUME_STEP, default=DEFAULT_VOLUME_STEP): cv.positive_int,
-    vol.Optional(CONF_TURN_ON_ACTION, default=None): cv.SCRIPT_SCHEMA,
-    vol.Optional(CONF_TURN_OFF_ACTION, default=None): cv.SCRIPT_SCHEMA})
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_HOST): cv.string,
+        vol.Optional(CONF_USERNAME): cv.string,
+        vol.Optional(CONF_PASSWORD): cv.string,
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+        vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
+        vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
+        vol.Optional(CONF_VOLUME_STEP, default=DEFAULT_VOLUME_STEP): cv.positive_int,
+        vol.Optional(CONF_TURN_ON_ACTION, default=None): cv.SCRIPT_SCHEMA,
+        vol.Optional(CONF_TURN_OFF_ACTION, default=None): cv.SCRIPT_SCHEMA,
+    }
+)
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Foobar Player platform."""
     from pyfoobar2k import FoobarRemote
+
     name = config.get(CONF_NAME)
     host = config.get(CONF_HOST)
     port = config.get(CONF_PORT)
@@ -72,43 +97,49 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
     remote = FoobarRemote(host, port, username, password, timeout)
 
-    add_devices([FoobarDevice(hass, remote, name, volume_step, turn_on_action, turn_off_action)])
+    add_devices(
+        [FoobarDevice(hass, remote, name, volume_step, turn_on_action, turn_off_action)]
+    )
 
 
 class FoobarDevice(MediaPlayerEntity):
-
-    def __init__(self, hass, remote, name, volume_step, turn_on_action=None, turn_off_action=None):
-
+    def __init__(
+        self, hass, remote, name, volume_step, turn_on_action=None, turn_off_action=None
+    ):
         self._name = name
         self._remote = remote
         self.hass = hass
         self._volume = 0.0
-        self._track_name = ''
-        self._track_artist = ''
-        self._track_album_name = ''
+        self._track_name = ""
+        self._track_artist = ""
+        self._track_album_name = ""
         self._track_duration = 0
         self._track_position = 0
         self._track_position_updated_at = None
-        self._albumart = ''
-        self._current_playlist = ''
+        self._albumart = ""
+        self._current_playlist = ""
         self._playlists = []
         self._shuffle = 0
         self._volume_step = volume_step
         self._selected_source = None
         self._state = STATE_UNKNOWN
         self._base_url = self._remote.url
-        self._albumart_path = ''
+        self._albumart_path = ""
         # Script creation for the turn on/off config options
         if turn_on_action is not None:
             turn_on_action = script.Script(
-                self.hass, turn_on_action,
+                self.hass,
+                turn_on_action,
                 "{} turn ON script".format(self.name),
-                self.async_update_ha_state(True))
+                self.async_update_ha_state(True),
+            )
         if turn_off_action is not None:
             turn_off_action = script.Script(
-                self.hass, turn_off_action,
+                self.hass,
+                turn_off_action,
                 "{} turn OFF script".format(self.name),
-                self.async_update_ha_state(True))
+                self.async_update_ha_state(True),
+            )
 
         self._turn_on_action = turn_on_action
         self._turn_off_action = turn_off_action
@@ -117,9 +148,9 @@ class FoobarDevice(MediaPlayerEntity):
         try:
             info = self._remote.state()
             if info:
-                if info['isPlaying'] == '1':
+                if info["isPlaying"] == "1":
                     self._state = STATE_PLAYING
-                elif info['isPaused'] == '1':
+                elif info["isPaused"] == "1":
                     self._state = STATE_PAUSED
                 else:
                     self._state = STATE_IDLE
@@ -128,22 +159,24 @@ class FoobarDevice(MediaPlayerEntity):
             self.schedule_update_ha_state()
 
             if self._state in [STATE_PLAYING, STATE_PAUSED]:
-                self._track_name = info['title']
-                self._track_artist = info['artist']
-                self._track_album_name = info['album']
-                self._volume = int(info['volume']) / 100
-                self._shuffle = info['playbackorder']
-                self._track_duration = int(info['itemPlayingLen'])
-                self._albumart_path = info['albumArt']
-                self._track_position = int(info['itemPlayingPos'])
+                self._track_name = info["title"]
+                self._track_artist = info["artist"]
+                self._track_album_name = info["album"]
+                self._volume = int(info["volume"]) / 100
+                self._shuffle = info["playbackorder"]
+                self._track_duration = int(info["itemPlayingLen"])
+                self._albumart_path = info["albumArt"]
+                self._track_position = int(info["itemPlayingPos"])
                 self._track_position_updated_at = dt_util.utcnow()
 
             if self._state in [STATE_PLAYING, STATE_PAUSED, STATE_IDLE]:
                 sources_info = self._remote.playlist()
                 if sources_info:
-                    current_playlist_position = int(sources_info['playlistActive'])
-                    playlists_raw = sources_info['playlists']
-                    self._current_playlist = playlists_raw[current_playlist_position]['name']
+                    current_playlist_position = int(sources_info["playlistActive"])
+                    playlists_raw = sources_info["playlists"]
+                    self._current_playlist = playlists_raw[current_playlist_position][
+                        "name"
+                    ]
                     self._playlists = [item["name"] for item in playlists_raw]
                 else:
                     _LOGGER.warning("Updating %s sources failed:", self._name)
@@ -179,7 +212,7 @@ class FoobarDevice(MediaPlayerEntity):
     @property
     def media_content_type(self):
         """Content type of current playing media."""
-        return MEDIA_TYPE_MUSIC
+        return MediaType.MUSIC
 
     @property
     def media_title(self):
@@ -201,9 +234,9 @@ class FoobarDevice(MediaPlayerEntity):
         """Flag media player features that are supported."""
         supported_features = SUPPORT_FOOBAR_PLAYER
         if self._turn_on_action is not None:
-            supported_features |= SUPPORT_TURN_ON
+            supported_features |= MediaPlayerEntityFeature.TURN_ON
         if self._turn_off_action is not None:
-            supported_features |= SUPPORT_TURN_OFF
+            supported_features |= MediaPlayerEntityFeature.TURN_OFF
         return supported_features
 
     def turn_on(self):
@@ -222,44 +255,44 @@ class FoobarDevice(MediaPlayerEntity):
 
     def media_play_pause(self):
         """Send the media player the command for play/pause."""
-        self._remote.cmd('PlayOrPause')
+        self._remote.cmd("PlayOrPause")
 
     def media_pause(self):
         """Send the media player the command for play/pause if playing."""
         if self._state == STATE_PLAYING:
-            self._remote.cmd('PlayOrPause')
+            self._remote.cmd("PlayOrPause")
 
     def media_stop(self):
         """Send the media player the stop command."""
-        self._remote.cmd('Stop')
+        self._remote.cmd("Stop")
 
     def media_play(self):
         """Send the media player the command to play at the current playlist."""
-        self._remote.cmd('Start')
+        self._remote.cmd("Start")
 
     def media_previous_track(self):
         """Send the media player the command for prev track."""
-        self._remote.cmd('StartPrevious')
+        self._remote.cmd("StartPrevious")
 
     def media_next_track(self):
         """Send the media player the command for next track."""
-        self._remote.cmd('StartNext')
+        self._remote.cmd("StartNext")
 
     def set_volume_level(self, volume):
         """Send the media player the command for setting the volume."""
-        self._remote.cmd('Volume', int(volume * 100))
+        self._remote.cmd("Volume", int(volume * 100))
 
     def volume_up(self):
         """Send the media player the command for volume down."""
-        self._remote.cmd('VolumeDelta', self._volume_step)
+        self._remote.cmd("VolumeDelta", self._volume_step)
 
     def volume_down(self):
         """Send the media player the command for volume down."""
-        self._remote.cmd('VolumeDelta', -self._volume_step)
+        self._remote.cmd("VolumeDelta", -self._volume_step)
 
     def mute_volume(self, mute):
         """Mute the volume."""
-        self._remote.cmd('VolumeMuteToggle')
+        self._remote.cmd("VolumeMuteToggle")
 
     @property
     def media_position_updated_at(self):
@@ -282,13 +315,13 @@ class FoobarDevice(MediaPlayerEntity):
 
     def media_seek(self, position):
         """Send the media player the command to seek in current playing media."""
-        self._remote.cmd('SeekSecond', position)
+        self._remote.cmd("SeekSecond", position)
 
     @property
     def media_image_url(self):
         """Image url of current playing media."""
-        if 'cover_not_available' not in self._albumart_path:
-            self._albumart = '{}/{}'.format(self._base_url, self._albumart_path)
+        if "cover_not_available" not in self._albumart_path:
+            self._albumart = "{}/{}".format(self._base_url, self._albumart_path)
         return self._albumart
 
     @property
@@ -303,14 +336,16 @@ class FoobarDevice(MediaPlayerEntity):
 
     def select_source(self, source):
         """Select input source."""
-        playlists = self._remote.playlist()['playlists']
-        source_position = [index for index, item in enumerate(playlists) if item['name'] == source][0]
+        playlists = self._remote.playlist()["playlists"]
+        source_position = [
+            index for index, item in enumerate(playlists) if item["name"] == source
+        ][0]
         """ignoring first playlist in playlists index"""
         if source_position == 0:
             return None
         if source_position is not None:
-            self._remote.cmd('SwitchPlaylist', source_position)
-            self._remote.cmd('Start', 0)
+            self._remote.cmd("SwitchPlaylist", source_position)
+            self._remote.cmd("Start", 0)
             self._current_playlist = source
 
     @property
@@ -321,7 +356,7 @@ class FoobarDevice(MediaPlayerEntity):
     def set_shuffle(self, shuffle):
         """Send the media player the command to enable/disable shuffle mode."""
         playback_order = 4 if shuffle else 0
-        self._remote.cmd('PlaybackOrder', playback_order)
+        self._remote.cmd("PlaybackOrder", playback_order)
 
     @property
     def shuffle(self):
